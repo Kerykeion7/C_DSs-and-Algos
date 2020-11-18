ce668c0df38325e1a7fadf8b6b842633b5d9cc20
Jens D <jensdeweirdt@outlook.be>
2020-11-18 17:50:12 +0100

    Rename namespace

--- C_DSs-and-Algos.test/Arrays/DynamicArrayTest.cpp
+++ C_DSs-and-Algos.test/Arrays/DynamicArrayTest.cpp
@@ -4,9 +4,9 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace CDSsandAlgostest
+namespace DynamicArrayTest
 {
-	TEST_CLASS(CDSsandAlgostest)
+	TEST_CLASS(DynamicArrayTest)
 	{
 	public:
 

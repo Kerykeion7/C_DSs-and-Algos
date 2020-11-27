9b28f32c3237be1ed5cc1963735e809da827f615
Jens D <jensdeweirdt@outlook.be>
2020-11-27 13:09:52 +0100

    Add insertion func & tests

--- C_DSs-and-Algos.proj/BSTs/BinarySearchTree.c
+++ C_DSs-and-Algos.proj/BSTs/BinarySearchTree.c
@@ -5,11 +5,49 @@ BinarySearchTree* init_bin_srch_tree()
 {
 	BinarySearchTree* tree = (BinarySearchTree*)malloc(sizeof(BinarySearchTree));
 	if (!tree) return NULL;
+	tree->Root = NULL;
 	return tree;
+}
+
+BinarySearchTreeNode* traverse_and_return_parent(BinarySearchTreeNode* node, int value) {
+	if (node->value == value) return NULL;
+	if (!node->RightChild || !node->LeftChild) return node;
+	if (value < node->value) return traverse_and_return_parent(node->LeftChild, value);
+	return traverse_and_return_parent(node->RightChild, value);
+}
+
+void set_bin_srch_tree_root(BinarySearchTree* tree, int value) {
+	tree->Root = (BinarySearchTreeNode*)malloc(sizeof(BinarySearchTreeNode));
+	if (!tree->Root) exit(EXIT_FAILURE);
+	tree->Root->value = value;
+	tree->Root->Parent = tree->Root;
+	tree->Root->LeftChild = NULL;
+	tree->Root->RightChild = NULL;
+}
+
+BinarySearchTreeNode* set_new_tree_node(BinarySearchTreeNode* parent, int value) {
+	BinarySearchTreeNode* newNode = (BinarySearchTreeNode*)malloc(sizeof(BinarySearchTreeNode));
+	if (!newNode) exit(EXIT_FAILURE);
+	newNode->Parent = parent;
+	newNode->value = value;
+	newNode->LeftChild = NULL;
+	newNode->RightChild = NULL;
+	return newNode;
 }
 
 void insert_tree_node(BinarySearchTree* tree, int value)
 {
+	if (!tree->Root) {
+		set_bin_srch_tree_root(tree, value);
+		return;
+	}
+
+	BinarySearchTreeNode* parent = traverse_and_return_parent(tree->Root, value);
+	if (!parent) return;
+
+	BinarySearchTreeNode* newNode = set_new_tree_node(parent, value);
+	if (value < parent->value) parent->LeftChild = newNode;
+	if (value > parent->value) parent->RightChild = newNode;
 }
 
 int get_tree_node_count(BinarySearchTree* tree)
--- C_DSs-and-Algos.test/BSTs/BinarySearchTreeTest.cpp
+++ C_DSs-and-Algos.test/BSTs/BinarySearchTreeTest.cpp
@@ -9,9 +9,48 @@ namespace BinarySearchTreeTest
 	TEST_CLASS(BinarySearchTreeTest)
 	{
 	public:
-		TEST_METHOD(TestNotNull) {
+		TEST_METHOD(TestIsNull) {
 			BinarySearchTree* tree = init_bin_srch_tree();
 			Assert::IsNotNull(tree);
+			Assert::IsNull(tree->Root);
+		}
+
+		TEST_METHOD(TestFirstInsertion) {
+			BinarySearchTree* tree = init_bin_srch_tree();
+			Assert::IsNull(tree->Root);
+			insert_tree_node(tree, 5);
+			Assert::IsNotNull(tree->Root);
+			Assert::IsTrue(tree->Root == tree->Root->Parent);
+			Assert::IsTrue(tree->Root->value == 5);
+			Assert::IsNull(tree->Root->LeftChild);
+			Assert::IsNull(tree->Root->RightChild);
+		}
+
+		TEST_METHOD(TestInsertion) {
+			BinarySearchTree* tree = init_bin_srch_tree();
+			insert_tree_node(tree, 5);
+			insert_tree_node(tree, 3);
+			Assert::IsNotNull(tree->Root->LeftChild);
+			insert_tree_node(tree, 8);
+			Assert::IsNotNull(tree->Root->RightChild);
+			Assert::IsTrue(tree->Root->LeftChild->value == 3);
+			Assert::IsTrue(tree->Root->RightChild->value == 8);
+			Assert::IsTrue(tree->Root->RightChild->Parent == tree->Root 
+				&& tree->Root->LeftChild->Parent == tree->Root);
+			insert_tree_node(tree, 9);
+			insert_tree_node(tree, 6);
+			Assert::IsTrue(tree->Root->RightChild->RightChild->value = 9);
+			Assert::IsTrue(tree->Root->RightChild->LeftChild->value = 6);
+			insert_tree_node(tree, 1);
+			insert_tree_node(tree, 4);
+			insert_tree_node(tree, 1);
+			insert_tree_node(tree, 4);
+			Assert::IsTrue(tree->Root->LeftChild->LeftChild->value == 1);
+			Assert::IsTrue(tree->Root->LeftChild->RightChild->value == 4);
+			insert_tree_node(tree, 2);
+			insert_tree_node(tree, -1);
+			Assert::IsTrue(tree->Root->LeftChild->LeftChild->RightChild->value == 2);
+			Assert::IsTrue(tree->Root->LeftChild->LeftChild->LeftChild->value == -1);
 		}
 	};
 }
